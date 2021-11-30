2e84b71f0e263b2900d502bf6c125a27ca6a54d6
coder12e4 <coderacademykeral@gmail.com>
2021-11-30 12:51:12 +0530

    listview and grid view

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:mysecondproject/pages/gridview.dart';
 import 'package:mysecondproject/pages/homepage.dart';
-void main()
-{
+
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home:loginpage() ,
-    theme: ThemeData(primarySwatch: Colors.blue),);
+    return MaterialApp(
+      home: gridview(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
   }
 }
+
 class loginpage extends StatefulWidget {
   const loginpage({Key? key}) : super(key: key);
   @override
@@ -20,8 +25,8 @@ class loginpage extends StatefulWidget {
 }
 
 class _loginpageState extends State<loginpage> {
-  TextEditingController obj_username=TextEditingController();
-  TextEditingController obj_password=TextEditingController();
+  TextEditingController obj_username = TextEditingController();
+  TextEditingController obj_password = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -32,75 +37,73 @@ class _loginpageState extends State<loginpage> {
         color: HexColor("01B0AB"),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-         // mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
               margin: EdgeInsets.only(top: 60),
-              child: Text("USER_LOGIN",style: TextStyle(
-                color: Colors.white,fontSize: 26,fontWeight: FontWeight.bold
-              ),),
+              child: Text(
+                "USER_LOGIN",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold),
+              ),
             ),
-                Container(
-                    height: 70,
-                    width: 400,
-                    margin: EdgeInsets.only(top: 40),
-                    padding: EdgeInsets.only(top:20),
-                    child: Center(
-                      child: TextField(
-                        controller: obj_username,
-                        onChanged: (val){},
-                        decoration: InputDecoration(
-                           border: InputBorder.none,
-                            hintText: "Username ",
-                            hintStyle: TextStyle(fontSize: 16,color: Colors.black),
-                            filled: true,
-                            fillColor: Colors.white,
-                            contentPadding: EdgeInsets.only(left: 14,top: 14,bottom: 14),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20),
-                                borderSide: BorderSide(color: Colors.black26)
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.black)
-                            )
-                        ),
-                      ),
-                    )
-                ),
             Container(
                 height: 70,
                 width: 400,
-                margin: EdgeInsets.only(top: 40,bottom: 30),
+                margin: EdgeInsets.only(top: 40),
+                padding: EdgeInsets.only(top: 20),
+                child: Center(
+                  child: TextField(
+                    controller: obj_username,
+                    onChanged: (val) {},
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Username ",
+                        hintStyle: TextStyle(fontSize: 16, color: Colors.black),
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding:
+                            EdgeInsets.only(left: 14, top: 14, bottom: 14),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide(color: Colors.black26)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black))),
+                  ),
+                )),
+            Container(
+                height: 70,
+                width: 400,
+                margin: EdgeInsets.only(top: 40, bottom: 30),
                 //padding: EdgeInsets.only(top:20),
                 child: Center(
                   child: TextField(
                     controller: obj_password,
-                    onChanged: (val){},
+                    onChanged: (val) {},
                     decoration: InputDecoration(
                         border: InputBorder.none,
                         hintText: "Password",
-                        hintStyle: TextStyle(fontSize: 16,color: Colors.black),
+                        hintStyle: TextStyle(fontSize: 16, color: Colors.black),
                         filled: true,
                         fillColor: Colors.white,
-                        contentPadding: EdgeInsets.only(left: 14,top: 14,bottom: 14),
+                        contentPadding:
+                            EdgeInsets.only(left: 14, top: 14, bottom: 14),
                         focusedBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(20),
-                            borderSide: BorderSide(color: Colors.black26)
-                        ),
+                            borderSide: BorderSide(color: Colors.black26)),
                         enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black)
-                        )
-                    ),
+                            borderSide: BorderSide(color: Colors.black))),
                   ),
-                )
-            ),
+                )),
             GestureDetector(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder:
-                (context)=>homepage()));
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => homepage()));
               },
               child: Container(
-                margin: EdgeInsets.only(top: 30,left: 10),
+                margin: EdgeInsets.only(top: 30, left: 10),
                 //padding: EdgeInsets.only(top: 20),
                 height: 50,
                 width: 385,
@@ -110,19 +113,19 @@ class _loginpageState extends State<loginpage> {
                   // border: Border.all(color: Colors.red, width: 2),
                 ),
                 child: Center(
-                  child: Text("LOG IN",style: TextStyle(
-                      color: HexColor("01B0AB"),fontSize: 20,
-                      fontWeight: FontWeight.bold
-                  ),),
+                  child: Text(
+                    "LOG IN",
+                    style: TextStyle(
+                        color: HexColor("01B0AB"),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
             ),
-
           ],
-
         ),
       ),
     );
   }
 }
-
